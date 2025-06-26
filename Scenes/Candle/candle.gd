01873d592398aa2498cba42814e58eb78b610ab2
QWEDt <64944732+QWEDt@users.noreply.github.com>
2025-06-26 17:13:03 +0500

    Qwed (#2)
    * Fire particles
    
    * Candle using AnimationPlayer instead of code imp

--- Scenes/Candle/candle.gd
+++ Scenes/Candle/candle.gd
@@ -1,35 +1,57 @@
 extends StaticBody3D
 
 
-@export var stages : Array[ArrayMesh]
-
 var damage := 5
+var is_active := true
 
 
 func _interact() -> void:
-	if not has_meta("health"):
-		print("HelthComponent not found on class " + self.get_class())
-		return
+	var health_component = get_component("health") as HealthComponent
 	
-	var health_component = get_meta("health") as HealthComponent
-	health_component.restore_health()
+	if health_component.current_health <= 0:
+		health_component.restore_health()
+	
+	var timer_component = get_component("timer") as TimerComponent
+	timer_component.start()
+	turn_fire(true)
 
 
 func _on_timer_component_timeout() -> void:
-	if not has_meta("health"):
-		print("HelthComponent not found on class " + self.get_class())
+	if not is_active:
 		return
 	
-	var health_component = get_meta("health") as HealthComponent
+	var health_component = get_component("health") as HealthComponent
 	health_component.change_health(-damage)
+	
+	if randi_range(1, 5) == 1:
+		turn_fire(false)
+		var timer_component = get_component("timer") as TimerComponent
+		timer_component.stop()
 
 
 func _on_health_component_health_updated(new_health: int) -> void:
 	if new_health > 80:
-		%CandleMesh.mesh = stages[0]
+		%AnimationPlayer.play("state_0")
 	elif new_health > 50:
-		%CandleMesh.mesh = stages[1]
+		%AnimationPlayer.play("state_1")
 	elif new_health > 20:
-		%CandleMesh.mesh = stages[2]
+		%AnimationPlayer.play("state_2")
 	else:
-		%CandleMesh.mesh = stages[3]
+		%AnimationPlayer.play("state_3")
+
+
+func get_component(component_name: String) -> BaseComponent:
+	if not has_meta(component_name):
+		print(component_name + " not found on class " + str(self.get_class()))
+		return null
+	
+	return get_meta(component_name)
+
+
+func turn_fire(state: bool) -> void:
+	is_active = state
+	%FireParticles.emitting = state
+
+
+func _on_health_component_death() -> void:
+	turn_fire(false)
